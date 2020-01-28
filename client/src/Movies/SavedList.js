81f1d7e11714fc1db4fa3c8f55f7d21d7a903113
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-01-28 14:37:54 -0500

    useHistory hook to create the button from Home

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
 
-const SavedList = props => (
-  <div className="saved-list">
+
+const SavedList = props => {
+
+  const history = useHistory();
+
+  const routeToHome = () => {
+    history.push('/')
+  }
+
+  return (<div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
       <span className="saved-movie"><Link to={`/movie/${movie.id}`}>{movie.title}</Link></span>
     ))}
-    <div className="home-button"><Link to='/'>Home</Link></div>
+    <div className="home-button" onClick={routeToHome}>Home</div>
   </div>
-);
+  
+  );
+  }
+
 
 export default SavedList;
